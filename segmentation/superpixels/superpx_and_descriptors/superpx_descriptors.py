3921c4216327515b99c03089f1c2382f8f239492
Zac <codeohms@protonmail.com>
2022-08-09 00:37:40 +1000

    Added Jaccard Similarity descriptor, but doesn't work too well due to padding pixels placed around the irregularly shaped region. It's also VERY slow so not worth trying to implement it correctly.

--- segmentation/superpixels/superpx_and_descriptors/superpx_descriptors.py
+++ segmentation/superpixels/superpx_and_descriptors/superpx_descriptors.py
@@ -1,20 +1,72 @@
 import numpy as np
+# from scipy.spatial import distance
+from sklearn.metrics import jaccard_score
+from skimage.transform import resize
+from skimage.util import img_as_ubyte
+import cv2 as cv
+import matplotlib.pyplot as plt
 
 ####
 #### Generate descriptor image
-def gen_discriptor_img(superpx_img, img, descr_func):
+def gen_discriptor_img(superpx_img, img, descr_func, descr_func_args=[None]):
     descriptors = np.zeros((superpx_img.max()+1,3))
     im_descriptors = np.zeros_like(img)
 
+    # for (i, segVal) in enumerate(np.unique(superpx_img)):
+    #     mask = np.zeros(img.shape[:2], dtype = "uint8")
+    #     mask[superpx_img == segVal] = 255
+    #     print(i, segVal)
+    #     plt.figure()
+    #     plt.imshow(mask)
+    #     plt.figure()
+    #     plt.imshow(img[superpx_img==i])
+    #     plt.show()
+
+    # for i in range(superpx_img.min(), superpx_img.max()+1):
+    #     args = [img[superpx_img==i]] + descr_func_args
+    #     plt.imshow(img[superpx_img==i])
+    #     plt.show()
+    #     descriptors[i] = descr_func(args)
+    #     im_descriptors[superpx_img==i] = descriptors[i]
+
+    # for i in range(superpx_img.min(), superpx_img.max()+1):
+    #     mask = np.zeros(img.shape[:2], dtype = "uint8")
+    #     mask[superpx_img==i] = 255
+    #     args = [cv.bitwise_and(img, img, mask=mask)] + descr_func_args
+    #     descriptors[i] = descr_func(args)
+    #     im_descriptors[superpx_img==i] = descriptors[i]
+
     for i in range(superpx_img.min(), superpx_img.max()+1):
-        descriptors[i] = descr_func(img[superpx_img==i])
+        args = [img, superpx_img==i] + descr_func_args
+        descriptors[i] = descr_func(args)
         im_descriptors[superpx_img==i] = descriptors[i]
 
     return im_descriptors
 
 ####
 #### Avg. RGB descriptor
-def avg_rgb_descriptor(region):
+def avg_rgb_descriptor(args):
+    img = args[0]
+    superpx_img_indicies = args[1]
+
+    # Region as a column of rgb pairs:
+    region = img[superpx_img_indicies]
     return [region[:,0].mean(), region[:,1].mean(), region[:,2].mean()]
+
+####
+#### Jaccard Similarity descriptor
+def jaccard_descriptor(args):
+    img = args[0]
+    superpx_img_indicies = args[1]
+    comp_img = args[2]
+
+    mask = np.zeros(img.shape[:2], dtype = "uint8")
+    mask[superpx_img_indicies] = 255
+    region = cv.bitwise_and(img, img, mask=mask)
+    # Resize comp_img to region size if needed:
+    if comp_img.shape != region.shape:
+        comp_img = resize(comp_img.ravel(), region.shape)
+        comp_img = img_as_ubyte(comp_img)
+    return jaccard_score(comp_img.ravel(), region.ravel(), average="micro")
 
 ####
--- segmentation/superpixels/superpx_and_descriptors/superpxs_w_descriptors.py
+++ segmentation/superpixels/superpx_and_descriptors/superpxs_w_descriptors.py
@@ -14,8 +14,9 @@ if __name__ == '__main__':
     script_dir = os.path.dirname(getsourcefile(lambda:0))
     os.chdir(script_dir)
     os.chdir('../../../')
+    imgs_dir = 'test_media/imgs/'
 
-    img = imread('test_media/imgs/test_img_0.jpg')
+    img = imread(imgs_dir+'test_img_0.jpg')
 
     # Apply superpixel algos:
     superpx_img = None
@@ -28,11 +29,17 @@ if __name__ == '__main__':
     # Avg. RGB:
     descr_imgs.append(gen_discriptor_img(superpx_img, img, avg_rgb_descriptor))
 
+    # Jaccard Similarity:
+    # sample_comp_img = imread(imgs_dir+'sample_jacc_comp.png')
+    # descr_imgs.append(gen_discriptor_img(superpx_img, img, jaccard_descriptor, [sample_comp_img]))
+
     descr_imgs_num = len(descr_imgs)
-    if descr_imgs_num < 2:
-        plt.imshow(mark_boundaries(descr_imgs[0], superpx_img))
+    if descr_imgs_num < 1:
+        plt.imshow(mark_boundaries(descr_imgs[0], superpx_img)) 
     else:
         figs_num_hor = math.ceil(descr_imgs_num/2)
+        if figs_num_hor == 1:
+            figs_num_hor = 2
         figure_descr_imgs, ax = plt.subplots(2, figs_num_hor, figsize=(10, 10), sharex=True, sharey=True)
         for i_fig, fig in enumerate(descr_imgs):
             ax[i_fig%figs_num_hor, math.floor(i_fig/figs_num_hor)].imshow(mark_boundaries(descr_imgs[i_fig], superpx_img))
