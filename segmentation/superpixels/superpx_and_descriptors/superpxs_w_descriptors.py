b00a68838e7476f1c02eb83bf96f6d847989207c
Zac <codeohms@protonmail.com>
2022-09-12 23:52:35 +1000

    Fixing bugs in the superpixel descriptor demo.

--- segmentation/superpixels/superpx_and_descriptors/superpxs_w_descriptors.py
+++ segmentation/superpixels/superpx_and_descriptors/superpxs_w_descriptors.py
@@ -28,6 +28,9 @@ def apply_superpx_descriptors(superpx_img, img):
     # sample_comp_img = skio.imread(imgs_dir+'sample_jacc_comp.png')
     # descr_imgs.append(gen_discriptor_img(superpx_img, img, jaccard_descriptor, [sample_comp_img]))
 
+    # Avg. RGB:
+    descr_imgs.append(gen_discriptor_img(superpx_img, img, avg_rgb_descriptor))
+
     # Dominant Colour:
     # descr_imgs.append(gen_discriptor_img(superpx_img, img, dominant_colour_descriptor))
     dom_colours_num = 2
@@ -39,28 +42,29 @@ def apply_superpx_descriptors(superpx_img, img):
         i += 3
 
     # Hue and Saturation average and MAD (mean absolute deviation)
-    hue_mean_sat_mean_hue_MAD_sat_MAD = gen_discriptor_img(superpx_img, img, hs_stats_descriptor, descr_dims=4)
+    hue_mean_sat_mean_hue_MAD_sat_MAD = gen_discriptor_img(superpx_img, img, hs_stats_descriptor, img_dtype=np.int32, descr_dims=6)
     descr_imgs.append(hue_mean_sat_mean_hue_MAD_sat_MAD[:,:,:3])
-    descr_imgs.append(np.delete(hue_mean_sat_mean_hue_MAD_sat_MAD, 2, 2))
-
-    # Avg. RGB:
-    descr_imgs.append(gen_discriptor_img(superpx_img, img, avg_rgb_descriptor))
+    # descr_imgs.append(np.delete(hue_mean_sat_mean_hue_MAD_sat_MAD, 2, 2))
+    descr_imgs.append(hue_mean_sat_mean_hue_MAD_sat_MAD[:,:,3:])
 
     return descr_imgs
 
 def disp_descr_imgs(superpx_img, fig_and_ax, descr_imgs, descr_imgs_num):
-    fig = fig_and_ax.pop(0)
+    # fig = fig_and_ax.pop(0)
     if descr_imgs_num < 1:
-        fig.set_data(mark_boundaries(descr_imgs[0], superpx_img))
+        # fig.set_data(mark_boundaries(descr_imgs[0], superpx_img))
+        fig_and_ax[0].set_data(mark_boundaries(descr_imgs[0], superpx_img))
     else:
         figs_num_hor = math.ceil(descr_imgs_num/2)
         if figs_num_hor == 1:
             figs_num_hor = 2
-        for i_fig, fig in enumerate(descr_imgs):
+        for i_fig, fig in enumerate(fig_and_ax[1:]):
             # fig_and_ax[i_fig].set_data(mark_boundaries(descr_imgs[i_fig], superpx_img))
             asdf = descr_imgs[i_fig]
             asdfg = mark_boundaries(asdf, superpx_img)
-            fig_and_ax[i_fig].set_data(asdfg)
+            asdfg = asdfg.astype(asdf.dtype)
+            # fig_and_ax[i_fig].set_data(asdfg)
+            fig.set_data(asdfg)
 
         for a in ax.ravel():
             a.set_axis_off()
@@ -93,10 +97,11 @@ if __name__ == '__main__':
 
     plt.tight_layout()
 
-    descr_imgs_num = 5
 
     # Display descriptor images:
         # Setup figures:
+    descr_imgs_num = 5
+    colour_maps = ['rgb', 'rgb', 'rgb', 'cividis', 'cividis']
     descr_imgs_fig = None
     if descr_imgs_num < 1:
         descr_imgs_fig = [plt.figure()]
@@ -107,7 +112,15 @@ if __name__ == '__main__':
         descr_imgs_fig, ax = plt.subplots(2, figs_num_hor, sharex=True, sharey=True)
         descr_imgs_fig = [descr_imgs_fig]
         for i_fig in range(descr_imgs_num):
-            descr_imgs_fig.append(ax[i_fig%2, math.floor(0.5*i_fig)].imshow(img))
+            axis = ax[i_fig%2, math.floor(0.5*i_fig)]
+            colour_m = colour_maps[i_fig]
+            if colour_m == 'rgb':
+                descr_imgs_fig.append(axis.imshow(img))
+            else:
+                descr_imgs_fig.append(axis.imshow(img, vmin=0.0, vmax=360.0, cmap=colour_m))
+                # descr_imgs_fig.append(axis.pcolormesh(img, cmap=colour_m))
+                plt.colorbar(descr_imgs_fig[-1], ax=axis)
+                
     descr_imgs_fig[0].canvas.mpl_connect('close_event', on_close)
 
     while(do_loop):
