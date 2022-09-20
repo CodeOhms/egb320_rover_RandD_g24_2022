8bd5331bb5631d6e6e7e973bf7ea8b01abba5876
Zac H <n10204261@qut.edu.au>
2022-09-20 13:32:00 +1000

    Final PoC works well with new descriptor based on 25% quantile of the hue differences from a given hue.

--- segmentation/superpixels/superpx_PoC/superpxs_PoC.py
+++ segmentation/superpixels/superpx_PoC/superpxs_PoC.py
@@ -6,46 +6,79 @@ from skimage import segmentation
 from fast_slic.neon import SlicNeon as Slic
 from skimage.segmentation import mark_boundaries
 import cv2 as cv
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-def superpx_slic_trans(img):
+def superpx_slic_trans(img, num_regions=40):
     # slic = Slic(num_components=40, compactness=1, min_size_factor=0) # Supposedly gets FPS increase, but I don't see any...
-    slic = Slic(num_components=40, compactness=10)
+    slic = Slic(num_components=num_regions, compactness=10)
     assignment = slic.iterate(img) # Cluster Map
     return assignment
 
-def gen_superpx_img(img):
-    return superpx_slic_trans(img)
+def grid_superpx_trans(img):
+    n_cells_x = 15
+    n_cells_y = 10
+    size_x = img.shape[1]//n_cells_x
+    size_y = img.shape[0]//n_cells_y
+    print(size_x,size_y)
+
+    descriptors = np.zeros((n_cells_y,n_cells_x,3))
+    for y in range(n_cells_y):
+        for x in range(n_cells_x):
+            region = img[y*size_y:(y+1)*size_y,x*size_x:(x+1)*size_x]
+            # labels = 
+            descriptors[y,x] = get_descr(region)
+
+
+def gen_superpx_img(img, num_regions=40):
+    return superpx_slic_trans(img, num_regions)
+    # return grid_superpx_trans(img)
 
 def get_region1d(img, superpx_img_indicies):
     return img[superpx_img_indicies]
 
-def hs_stats_descriptor(args):
-    img = args[0]
-    superpx_img_indicies = args[1]
+def smallest_angle_between(angles0, angles1):
+# https://stackoverflow.com/questions/7570808/how-do-i-calculate-the-difference-of-two-angle-measures
+    phi = np.abs(angles1 - angles0) % 360
+    distance = phi
+    if phi > 180:
+        distance = 360 - phi
+    # print('angles')
+    # print(angles0, angles1, distance)
+    # print()
+    return distance
 
-    img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+def MAD_from_hue(args):
+    img_hsv = args[0]
+    superpx_img_indicies = args[1]
+    hue = args[3]
 
     # Region as a column of HSV pairs:
     region = get_region1d(img_hsv, superpx_img_indicies)
-    hue_avg = region[:,0].mean()
-    sat_avg = region[:,1].mean()
+    angles_between = np.array([smallest_angle_between(hue_px, hue) for hue_px in region[:,0]])
+    # hue_mad = np.mean(angles_between)
+    # hue_mad = np.median(angles_between)
+    hue_mad = np.quantile(angles_between, 0.25)
 
-    return (hue_avg, sat_avg)
+    return hue_mad
 
-def gen_discriptor_img(superpx_img, img, descr_func, descr_func_args=[None], descr_dims=3):
-    descriptors = np.zeros((superpx_img.max()+1, descr_dims))
-    im_descriptors = np.zeros((img.shape[0], img.shape[1], descr_dims), dtype=img.dtype)
+def gen_discriptor_img(superpx_img, img, descr_func, img_dtype=None, descr_func_args=[None], descr_dims=3):
+    if img_dtype == None:
+        img_dtype = img.dtype
+    
+    descriptors = [ ]
+    im_descriptors = np.zeros((img.shape[0], img.shape[1], descr_dims), dtype=img_dtype)
 
     for i in range(superpx_img.min(), superpx_img.max()+1):
-        args = [img, superpx_img==i] + descr_func_args
-        descriptors[i] = descr_func(args)
+        args = [img, superpx_img==i, descr_dims] + descr_func_args
+        descriptors.append(descr_func(args))
         im_descriptors[superpx_img==i] = descriptors[i]
 
-    return im_descriptors
+    return (im_descriptors, descriptors)
+    # return im_descriptors
 
 def grab_frame(capture, res):
     frame = capture.read()
+    # frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
     return frame
 
 loop = True
@@ -56,11 +89,19 @@ def on_close():
 def PoC(capture, cam_res):
     global loop
     
-    hue_range_gb = np.array([[0, 38], [150, 180]])
-    sat_deviation = 0.15
-    sat_mid_gb = 0.8125
-    sat_range_gb = np.array([round(255*(sat_mid_gb-sat_deviation)), round(255*(sat_mid_gb+sat_deviation))], dtype=np.uint8)
-    # sat_range_gb = np.array([128, 255], dtype=np.uint8)
+    # sat_deviation = 0.15
+    # sat_mid_gb = 0.8125
+    # sat_range_gb = np.array([round(255*(sat_mid_gb-sat_deviation)), round(255*(sat_mid_gb+sat_deviation))], dtype=np.uint8)
+
+    num_classes = 3 # Sample, obstacle, rock
+
+    sat_deviations = [0.15, 0.15, 0.15]
+    sat_mids = [0.02, 0.7, 0.85]
+    sat_ranges = np.array([
+        [sat_mids[i] - sat_deviations[i], sat_mids[i] + sat_deviations[i]] for i in range(num_classes)
+    ])
+    # hue_mads = [200, 320, 87] # Rock, sample, obstacle
+    hue_mads = [2, 110, 207] # Sample, obstacle, rock
 
     frame = grab_frame(capture, cam_res)
 
@@ -71,6 +112,14 @@ def PoC(capture, cam_res):
     dbug_img = np.zeros((100,512,3),np.uint8)
     font = cv.FONT_HERSHEY_SIMPLEX
 
+    empty_mask = np.zeros((frame.shape[:2]), dtype=np.uint8)
+    masks_shape = np.concatenate((frame.shape[:2], np.array([num_classes])))
+    masks_hue = np.zeros(masks_shape, dtype=np.uint8)
+    masks_sat = np.zeros(masks_shape, dtype=np.uint8)
+    masks_objs = np.zeros(masks_shape, dtype=np.uint8)
+    frame_masked_objs = np.zeros(masks_shape)
+
+    num_regions = 25
     while(loop):
         new_frame_time = time.time()
         
@@ -78,39 +127,73 @@ def PoC(capture, cam_res):
         frame = grab_frame(capture, cam_res)
 
     # Create superpixel image:
-        superpx_img = gen_superpx_img(frame)
+        superpx_img = gen_superpx_img(frame, num_regions)
 
     # Apply descriptors:
-        descr_img = gen_discriptor_img(superpx_img, frame, hs_stats_descriptor, descr_dims=2)
+        frame_f32 = np.float32(frame)
+        frame_hsv = cv.cvtColor(frame_f32, cv.COLOR_BGR2HSV_FULL)
+        frame_sat = frame_hsv[:,:,1]
+
+        # Hue MAD from given hue:
+        hue_mads_imgs_and_decrs = np.array([gen_discriptor_img(superpx_img, frame_hsv, MAD_from_hue, descr_func_args=[hue_mads[i]], descr_dims=1, img_dtype=np.float32)
+            for i in range(num_classes)
+        ])
+        # hue_mad_imgs = np.array([gen_discriptor_img(superpx_img, frame_hsv, MAD_from_hue, descr_func_args=[hue_mads[i]], descr_dims=1, img_dtype=np.float32)
+        #     for i in range(num_classes)
+        # ])
+        # hue_mad_imgs, hue_mad_descrs = hue_mads_imgs_and_decrs
+        # print('hue_mads_imgs_and_decrs shape')
+        # print(hue_mads_imgs_and_decrs.shape)
+        hue_mad_imgs = hue_mads_imgs_and_decrs[:,0]
+        # print('hue_mad_imgs shape')
+        # print(hue_mad_imgs.shape)
+        hue_mad_descrs = hue_mads_imgs_and_decrs[:,1]
 
     # Select descriptors to mask objects:
-        mask_hue_gb = cv.bitwise_or(cv.inRange(descr_img[:,:,0], int(hue_range_gb[0,0]), int(hue_range_gb[0,1])),
-            cv.inRange(descr_img[:,:,0], int(hue_range_gb[1,0]), int(hue_range_gb[1,1])))
-        mask_sat_gb = cv.inRange(descr_img[:,:,1], int(sat_range_gb[0]), int(sat_range_gb[1]))
-        mask_gb = cv.bitwise_and(mask_hue_gb, mask_sat_gb)
-        masked_gb = cv.bitwise_and(frame, frame, mask=mask_gb)
+        mad_threshold = 30
+        hue_mad_regions = np.zeros((1,3))
+        hue_mad_labels = hue_mad_descrs
+        for i_reg, reg_label in enumerate(np.unique(superpx_img)):
+            hue_mad_regions = np.array([hmad_lab[i_reg] for hmad_lab in hue_mad_labels])
+            reg_idxs = superpx_img == reg_label
+
+            hue_img_idx = np.argmin(hue_mad_regions)
+            print('region index', str(i_reg))
+            print()
+            if hue_mad_regions[hue_img_idx] < mad_threshold:
+                print(hue_mad_regions[hue_img_idx])
+                print()
+                masks_hue[:,:,hue_img_idx][reg_idxs] = 255
+            else:
+                print('Skipped ', str(hue_mad_regions[hue_img_idx]))
+                print()
     
-    # Find contours:
-        contours, hierarchy = cv.findContours(mask_gb, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    # # Find contours:
+    #     contours, hierarchy = cv.findContours(mask_gb, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-    # Find the convex hull object for each contour:
-        hull_list = []
-        for i in range(len(contours)):
-           hull = cv.convexHull(contours[i])
-           hull_list.append(hull)
+    # # Find the convex hull object for each contour:
+    #     hull_list = []
+    #     for i in range(len(contours)):
+    #        hull = cv.convexHull(contours[i])
+    #        hull_list.append(hull)
 
-    # Draw contours + hull results
-        contours_img = np.copy(frame)
-        cv.drawContours(contours_img, contours, -1, (5,255,0), 2) # Bright green
-        cv.drawContours(contours_img, hull_list, -1, (250,0,255), 2) # Pink/purple
+    # # Draw contours + hull results
+    #     contours_img = np.copy(frame)
+    #     cv.drawContours(contours_img, contours, -1, (5,255,0), 2) # Bright green
+    #     cv.drawContours(contours_img, hull_list, -1, (250,0,255), 2) # Pink/purple
 
     # Display results:
         cv.imshow("Frame", frame)
-        cv.imshow("Hue mask", mask_hue_gb)
-        cv.imshow("Saturation mask", mask_sat_gb)
-        cv.imshow("Superpixels", mark_boundaries(frame, superpx_img))
-        cv.imshow("Masked", masked_gb)
-        cv.imshow("Contours", contours_img)
+        cv.imshow("Sample hue mask", masks_hue[:,:,0])
+        cv.imshow("Obstacle hue mask", masks_hue[:,:,1])
+        cv.imshow("Rock hue mask", masks_hue[:,:,2])
+        # cv.imshow("Sample hue mask", masks_objs[:,:,0])
+        # cv.imshow("Obstacle hue mask", masks_objs[:,:,1])
+        # cv.imshow("Rock hue mask", masks_objs[:,:,2])
+        # cv.imshow("Saturation mask", mask_sat_gb)
+        # cv.imshow("Superpixels", mark_boundaries(frame, superpx_img))
+        # cv.imshow("Masked", masked_gb)
+        # cv.imshow("Contours", contours_img)
         key = cv.waitKey(1) & 0xFF
         #if the `q` key was pressed, break from the loop
         if key == ord("q"):
@@ -121,14 +204,30 @@ def PoC(capture, cam_res):
         cv.imshow("Debug message", dbug_img)
         prev_frame_time = new_frame_time
         dbug_img = np.copy(dbug_img_canvas)
-    
+        masks_hue[:,:,0] = np.copy(empty_mask)
+        masks_hue[:,:,1] = np.copy(empty_mask)
+        masks_hue[:,:,2] = np.copy(empty_mask)
+
+def init_camera(camera):
+# https://picamera.readthedocs.io/en/release-1.13/recipes1.html#capturing-consistent-images
+    # Set ISO to the desired value
+    camera.iso = 800
+    # Wait for the automatic gain control to settle
+    time.sleep(2)
+    # Now fix the values
+    camera.shutter_speed = camera.exposure_speed
+    camera.exposure_mode = 'off'
+    g = camera.awb_gains
+    camera.awb_mode = 'off'
+    camera.awb_gains = g
+
 if __name__ == "__main__":
     # initialize the video stream and allow the cammera sensor to warmup
     # Vertical res must be multiple of 16, and horizontal a multiple of 32
     cam_res = (128, 64)
-    video_stream = VideoStream(usePiCamera=True, resolution=cam_res, framerate=20).start()
+    video_stream = VideoStream(usePiCamera=True, resolution=cam_res, framerate=20, ).start()
     print("Camera warming up...")
-    time.sleep(2.0)
+    init_camera(video_stream.camera)
     
     PoC(video_stream, cam_res)
     
