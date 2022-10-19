f79cbb1cc5f277cdcfc6c4d2127dca3334df610d
Zac <n10204261@qut.edu.au>
2022-10-20 00:57:29 +1000

    Added a square wave for obstacles.

--- navigation/potential_fields_simplified/potential_fields.py
+++ navigation/potential_fields_simplified/potential_fields.py
@@ -9,31 +9,48 @@ class PF(object):
         self.x_data = np.linspace(b_min, b_max, num=num_samples)
         self.y_data = np.zeros_like(self.x_data)
     
-    def gen_potential_field(self, bearing, intensity=None, intensity_max=1):
+    def gen_potential_field(self, bearings, intensity=None, intensity_max=1):
         if intensity is None:
             intensity = intensity_max
-        self.y_data = self.pf_function(self.x_data, bearing, intensity, intensity_max)
+        pf_res = self.pf_function(self.x_data, bearings, intensity, intensity_max)
+        
+        # Clip results below zero and above intensity maximum:
+        pf_res[pf_res<0] = 0
+        pf_res[pf_res>intensity_max] = intensity_max
+        
+        self.y_data = pf_res
 
-def goal_potential_field(x_array, bearing, intensity, intensity_max):
+def goal_potential_field(x_array, bearings, intensity, intensity_max):
     '''
-    Piece-wise triangular function.
+    Piecewise triangular function.
     '''
+    
+    bearing = bearings[0]
     
     m = intensity_max/x_array.max() # Gradients
     c = (intensity - m*bearing, intensity + m*bearing)
-    pw_res = np.piecewise(x_array, [x_array < bearing, x_array >= bearing], [lambda x: m*x+c[0], lambda x: -m*x+c[1]])
+    return np.piecewise(x_array, [x_array < bearing, x_array >= bearing], [lambda x: m*x+c[0], lambda x: -m*x+c[1]])
 
-    # Clip results below zero and above intensity maximum:
-    pw_res[pw_res<0] = 0
-    pw_res[pw_res>intensity_max] = intensity_max
+def hazard_potential_field(x_array, bearings, intensity, intensity_max):
+    '''
+    Piecewise square function.
+    '''
     
-    return pw_res
+    b_min = bearings[0]
+    b_max = bearings[1]
+    return np.where(((b_min <= x_array) & (x_array <= b_max)), intensity, 0)
+    
 
 if __name__ == '__main__':
     goal_pf = PF(goal_potential_field)
+    haz_pf = PF(hazard_potential_field)
     
-    goal_pf.gen_potential_field(90.0)
+    goal_pf.gen_potential_field((90.0,), 0.5)
+    haz_pf.gen_potential_field((-140, -120), 0.7)
     
+    plt.figure()
     plt.plot(goal_pf.x_data, goal_pf.y_data)
+    plt.figure()
+    plt.plot(haz_pf.x_data, haz_pf.y_data)
     plt.show()
     
